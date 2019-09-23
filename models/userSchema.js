20adef37858d670f6c25c4d0ee758d7f3c9c8e1b
rashedInt32 <parvez08eee@gmail.com>
2019-09-23 09:36:04 +0600

    creates user schema

---
+++ models/userSchema.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import Joi from 'joi';
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    minlength: 5,
+    maxlength: 50,
+    required: true
+  },
+  lastName: {
+    type: String,
+    minlength: 5,
+    maxlength: 50,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  role: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongose.model('User', userSchema);
+
+const userValidate = (user) => {
+  const validateSchema = {
+    firstName: string().min(5).max(50).required(),
+    lastName: string().min(5).max(50).required(),
+    email: string().required().email(),
+    password: string().min(5).max(1024).required(),
+    role: string().required(),
+  }
+
+  return Joi.validate(user, validateSchema);
+}
+
+export { User, userValidate };
