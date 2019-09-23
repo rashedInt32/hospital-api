3add5602313b88bb99613028fae8ac083a5daaf8
rashedInt32 <parvez08eee@gmail.com>
2019-09-23 08:38:33 +0600

    used throng for clustaring

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import throng from 'throng';
 
 import { config } from '../config';
 
@@ -7,21 +8,27 @@ import { db } from '../db/connect';
 
 const dbUri = config.DB_URI;
 
-// Initialize express
-const app = express();
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+
+const startApp = () => {
+  // Initialize express
+  const app = express();
+
+  // Connect db
+  db.connect(dbUri, { useNewUrlParser: true });
+
+  // Initialize body parser
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+
+  // respond with "hello world" when a GET request is made to the homepage
+  app.get("/", (req, res) => res.json({ msg: "hello " }));
+
+  // PORT
+  const PORT = process.env.PORT || 3900;
+  // Listen server
+  app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`));
+};
 
 
-// Connect db
-db.connect(dbUri, {useNewUrlParser: true});
-
-// Initialize body parser
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => res.json({ msg: "hello " }));
-
-// PORT
-const PORT = process.env.PORT || 3900;
-// Listen server
-app.listen(PORT, () => console.log(`Backend listening on port ${PORT}`))
+throng(WORKERS, startApp);
