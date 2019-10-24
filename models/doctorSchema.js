cc037206895d1f92be62c109a447b3214d83280f
rashedInt32 <parvez08eee@gmail.com>
2019-10-24 14:16:01 +0600

    doctor schema

---
+++ models/doctorSchema.js
@@ -0,0 +1,98 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    minlength: 5,
+    maxlength: 50,
+    required: true
+  },
+  lastName: {
+    type: String,
+    minlength: 5,
+    maxlength: 50,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  hospital: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Hospital"
+  },
+  avatar: String,
+  specialties: [{type: String}],
+  availableDays: [{type: String}]
+});
+
+doctorSchema.methods.generateAuthToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      role: this.role,
+      hospital: this.hospital,
+      avatar: this.avatar
+    },
+    config.JWT_SECRET,
+    {}
+  );
+};
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+const userValidate = doctor => {
+  const validateSchema = {
+    firstName: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    lastName: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(50)
+      .required()
+      .email(),
+    password: Joi.string()
+      .min(5)
+      .max(1024)
+      .required(),
+    role: Joi.string().required(),
+    hospital: Joi.string().required(),
+    avatar: Joi.string(),
+    specialties: Joi.array(),
+    availableDays: Joi.array()
+  };
+
+  return Joi.validate(doctor, validateSchema);
+};
+
+const authValidate = doctor => {
+  const validateSchema = {
+    email: Joi.string()
+      .required()
+      .email(),
+    password: Joi.string().required()
+  };
+
+  return Joi.validate(doctor, validateSchema);
+};
+
+export { Doctor, userValidate, authValidate };
